8a013326dc63fcd5b7caf8bc7f70f29a8daa9c56
ANTOINE BADOC (1707409) <1707409@rgu.ac.uk>
2018-04-12 09:27:50 +0100

    server

--- login/servers.js
+++ login/servers.js
@@ -5,8 +5,10 @@ var app = express();
 
 app.use(require('express-session')({ resave: false, saveUninitialized: false, secret: 'a secret' }));
 app.use(steam.middleware({
-    realm: 'http://localhost:3000/',
-    verify: 'http://localhost:3000/verify',
+    //realm: 'http://localhost:3000/',
+    realm: 'https://quality-copper-8080.codio.io/',
+    //verify: 'http://localhost:3000/verify',
+    verify: 'https://quality-copper-8080.codio.io/verify',
     apiKey: process.argv[2]}
 ));
 
@@ -27,5 +29,5 @@ app.get('/logout', steam.enforceLogin('/'), function(req, res) {
     res.redirect('/');
 });
 
-app.listen(3000);
+app.listen(8080);
 console.log('listening');
